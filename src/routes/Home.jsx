0cc32e2ad4923de09826258d017dc064ef30534b
Makuzaza <maria.kuznetsova@edu.bc.fi>
2024-01-19 14:29:12 +0200

    second

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,27 @@
 import { Typography } from "@mui/material";
 import { url } from "../api/api";
 import ComponentA from "../components/ComponentA";
-import { getData } from "../services/apiRequests";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getDataRedux } from "../store/dataSlice";
 
-const data = await getData(url);
-
-console.log("Data: ", data);
+// const data = await getData(url);
 
 const Home = () => {
+    
+    const { data } = useSelector((state => state.data));
+    const dispatch = useDispatch();
+    
+    console.log("Data: ", data);
+
+    // useEffect(() => {   
+    //     dispatch(getDataRedux(url));
+    // }, [dispatch]);
+
+    useEffect(() => {   
+       if (data === undefined) dispatch(getDataRedux(url));
+    }, [data, dispatch]);
+
   return (
     <div>
       <Typography variant="h6" component="h2">
